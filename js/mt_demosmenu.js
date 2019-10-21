b534b55805c26ed381589949dcd681f509691c25
Vasileios Kechagias <vkechagias@gmail.com>
2019-10-21 12:56:34 +0300

    fix(js): do not include unnecessary js file and cleanup

--- js/mt_demosmenu.js
+++ js/mt_demosmenu.js
@@ -1,21 +1,13 @@
 /*
- * MTCountDown scripts
+ * MTDemosMenu scripts
  *  Add any module related script here
  */
 (function ($, Drupal, drupalSettings) {
 
-  Drupal.behaviors.mtCountdown = {
+  Drupal.behaviors.mtDemosMenu = {
     attach: function (context, settings) {
-      $(context).find('.mt-count-down-slot').once('mtCountdownInit').each(function() {
-        var expiration_date = drupalSettings.mt_demosmenu.expiration_date;
-        // Start countdown script.
-        $(this).countdown(expiration_date, function(event) {
-          $(this).html(event.strftime('' +
-            '<span class="mt-time"><span class="mt-count">%D</span> <span class="mt-label">'+Drupal.t('day%!d')+'</span></span> ' +
-            '<span class="mt-time"><span class="mt-count">%H</span> <span class="mt-label">'+Drupal.t('hours')+'</span></span> ' +
-            '<span class="mt-time"><span class="mt-count">%M</span> <span class="mt-label">'+Drupal.t('minutes')+'</span></span> ' +
-            '<span class="mt-time"><span class="mt-count">%S</span> <span class="mt-label">'+Drupal.t('seconds')+'</span></span>'));
-        });
+      $(context).find('.mt-demosmenu').once('mtDemosMenuInit').each(function() {
+
       });
     }
   };
